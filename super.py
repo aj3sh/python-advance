b068b4ea27b6d5855c3ef0ab4d2f5241ddab6aac
Ajesh Sen Thapa <aj3sshh@gmail.com>
2022-09-17 12:53:52 +0545

    Super and Type

---
+++ super.py
@@ -0,0 +1,15 @@
+class A:
+    def run(self):
+        print('run A')
+
+class B(A):
+    def run(self):
+        print('run B')
+
+def main():
+    b = B()
+    a = super(B, b) # based on MRO
+    a.run() # run A
+
+if __name__ == '__main__':
+    main()
---
+++ type.py
@@ -0,0 +1,39 @@
+class A:
+    x = 1
+
+    def __init__(self, y):
+        self.y = y
+
+    def run(self):
+        print(f'run A, x: {self.x}, y: {self.y}')
+
+
+# Making same class as of A using type
+
+def initB(b, y):
+    b.y = y
+
+def runB(b):
+    print(f'run B, x: {b.x}, y: {b.y}')
+
+B = type(
+    'B', # name of class
+    (), # base classes 
+    {   # attributes
+        'x': 1,
+        '__init__': initB,
+        'run': runB,
+    }
+)
+
+def main():
+    # running A
+    a = A(2)
+    a.run()
+
+    # running B
+    b = B(3)
+    b.run()
+
+if __name__ == '__main__':
+    main()
