42dc85e3242f65988d639ee89bc6e9664fd8a059
Ajesh Sen Thapa <aj3sshh@gmail.com>
2022-09-22 21:56:29 +0545

    exploring functions

---
+++ function.py
@@ -0,0 +1,8 @@
+def test():
+    pass
+
+method_code = "print('Hello world')"
+code_obj = compile(method_code, '_', 'exec')
+
+a = test.__class__(code_obj, globals())
+a()
---
+++ python_optimize_mode.py
@@ -0,0 +1,7 @@
+print("Hello world")
+
+if __debug__:
+    print("Debug mode")
+
+# python -O python_optimize_mode.py
+# python -OO python_optimize_mode.py
