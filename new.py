a3922bf1d2b29577ba078aaee6a84b7ec4c9604c
Ajesh Sen Thapa <aj3sshh@gmail.com>
2022-11-27 10:06:01 +0545

    __new__ example code

---
+++ new.py
@@ -0,0 +1,38 @@
+class Red:
+    def __str__(self):
+        return "RED"
+
+
+class Green:
+    def __str__(self):
+        return "GREEN"
+
+class Blue:
+    def __str__(self):
+        return "BLUE"
+
+class ColorFactory:
+    def __new__(cls, color):
+        if color.upper() == "RED":
+            return Red()
+        elif color.upper() == "GREEN":
+            return Green()
+        elif color.upper() == "BLUE":
+            return Blue()
+        raise Exception(f"Color {color} is not available.")
+    
+def main():
+    red = ColorFactory("RED")
+    print(red)
+
+    green = ColorFactory("Green")
+    print(green)
+
+    blue = ColorFactory("blue")
+    print(blue)
+
+    yellow = ColorFactory("yellow")
+    print(yellow)
+
+if __name__ == '__main__':
+    main()
--- python_optimize_mode.py
+++ python_optimize_mode.py
@@ -3,5 +3,6 @@ print("Hello world")
 if __debug__:
     print("Debug mode")
 
+# USAGE:
 # python -O python_optimize_mode.py
 # python -OO python_optimize_mode.py
